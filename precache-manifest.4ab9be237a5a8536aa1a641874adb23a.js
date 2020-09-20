f070a20231a528cd195e6beae7b7acdd41c500fc
Jonas Iggbom <jonas@curity.io>
2020-09-19 22:00:52 -0400

    Updates

---
+++ precache-manifest.4ab9be237a5a8536aa1a641874adb23a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825f203e9ccb42b8589d3a7ade6cb0ce",
+    "url": "/react-peloton/index.html"
+  },
+  {
+    "revision": "f7bf6ce40b68daa64584",
+    "url": "/react-peloton/static/js/2.2d3bbf7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-peloton/static/js/2.2d3bbf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163ac8d71fc18c64098c",
+    "url": "/react-peloton/static/js/main.a07296d7.chunk.js"
+  },
+  {
+    "revision": "13110e739c0d3237a08b",
+    "url": "/react-peloton/static/js/runtime-main.ec8184de.js"
+  }
+]);
